c84edbd08160cc8ca0cdfdad2526d13c42f42f64
mahdi <mahdi2082004@gmail.com>
2025-11-13 00:39:46 +0100

    feat(auth): add AuthController and AuthService for user registration

---
+++ src/main/java/com/pm/reddit_project/service/AuthService.java
@@ -0,0 +1,34 @@
+package com.pm.reddit_project.service;
+
+
+import com.pm.reddit_project.dto.RegisterRequest;
+import com.pm.reddit_project.model.User;
+import com.pm.reddit_project.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+
+@Service
+@AllArgsConstructor
+public class AuthService {
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+
+
+    @Transactional
+    public void signup(RegisterRequest registerRequest){
+        User user = new User();
+        user.setUsername(registerRequest.getUsername());
+        user.setEmail(registerRequest.getEmail());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+        user.setCreated(Instant.now());
+        user.setEnabled(false);
+
+        userRepository.save(user);
+
+    }
+}
