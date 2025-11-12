46956cbc848941a942d050bd499b5a78647f8134
mahdi <mahdi2082004@gmail.com>
2025-11-12 18:14:46 +0100

    Adding SecurityConfig

---
+++ src/main/java/com/pm/reddit_project/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.pm.reddit_project.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // Désactive la protection CSRF (utile pour les API REST)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll() // Autorise librement les routes d'authentification
+                        .anyRequest().authenticated() // Les autres requêtes nécessitent une authentification
+                );
+
+        return http.build();
+    }
+}
+
