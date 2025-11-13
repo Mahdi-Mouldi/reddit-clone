dd1c036e18805016cac633bf95d226303818419f
mahdi <mahdi2082004@gmail.com>
2025-11-13 01:13:11 +0100

    feat(auth): add AuthController and AuthService for user registration

--- src/main/java/com/pm/reddit_project/service/AuthService.java
+++ src/main/java/com/pm/reddit_project/service/AuthService.java
@@ -3,7 +3,9 @@ package com.pm.reddit_project.service;
 
 import com.pm.reddit_project.dto.RegisterRequest;
 import com.pm.reddit_project.model.User;
+import com.pm.reddit_project.model.VerificationToken;
 import com.pm.reddit_project.repository.UserRepository;
+import com.pm.reddit_project.repository.VerificationTokenRepository;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,12 +13,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
 public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
+    private final VerificationTokenRepository verificationTokenRepository;
 
 
     @Transactional
@@ -29,6 +33,16 @@ public class AuthService {
         user.setEnabled(false);
 
         userRepository.save(user);
+        String token = generateVerificationToken(user);
 
     }
+    private String generateVerificationToken(User user){
+        String token = UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+        verificationTokenRepository.save(verificationToken);
+        return token;
+    }
+
 }
